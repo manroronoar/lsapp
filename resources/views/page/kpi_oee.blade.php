7e41cf35638c110ff49913a3cdec48a65e0c5d00
manroronoar <manroronoars@gmail.com>
2019-12-12 17:27:17 +0700

    12/12/2019

--- resources/views/page/kpi_oee.blade.php
+++ resources/views/page/kpi_oee.blade.php
@@ -96,7 +96,7 @@
     jq(document).on('click', '.getmcname', function(event){
       event.preventDefault();
      var mcnumber = jq(this).attr('id'); 
-     var url = "{{ url('oeed') }}" +'/' + mcnumber 
+     var url = "{{ url('oeed') }}" +'/' + mcnumber;
      window.open(url, '_blank'); 
       // window.location.href  = url;
     
